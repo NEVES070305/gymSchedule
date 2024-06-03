834201a20da620076baab41bdd064dea8a1c0263
Guilherme Neves da Silva <guilhermenevesdasilva321@gmail.com>
2024-06-03 19:20:56 -0300

    delete: deleted files

--- GymScheduleBackend/Configuration/Settings.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.DataProtection;
-
-namespace GymScheduleBackend.Settings
-{
-    public static class Settings
-    {
-        public static string Secret="wshkxuzygfdwqcysguihdwkjnvb";
-    }
-}
--- GymScheduleBackend/Repository/ImplementationRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace GymScheduleBackend.Repository
-{
-    public class ImplementationRepository
-    {
-    }
-}
