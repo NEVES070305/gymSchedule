e12029bde3e28153bbf525882b11d98176a861d8
andrelbastida <andrelbastida@yahoo.com.br>
2024-06-03 20:16:42 -0300

    feat: Settings

---
+++ Backend/Configuration/Settings.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace Backend.Configuration
+{
+    public static class Settings
+    {
+        public static string Secret = "wshkxuzygfdwqcysguihdwkjnvb";
+    }
+}
+
