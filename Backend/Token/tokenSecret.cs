b8edfec320c7f29f03dd0d4b4f4396bff1b94c6e
andrelbastida <andrelbastida@yahoo.com.br>
2024-06-03 21:41:56 -0300

    feat: token

--- Backend/Token/tokenSecret.cs
+++ Backend/Token/tokenSecret.cs
@@ -17,7 +17,7 @@ namespace GymScheduleBackend.Services
             public static (string token, DateTime? expirationDate) GenerateToken(Pessoa user)
             {
                 var tokenHandler = new JwtSecurityTokenHandler(); //Classe que gera o token realmente
-                var key = Encoding.ASCII.GetBytes(Settings.Settings.Secret);
+                var key = Encoding.ASCII.GetBytes(Settings.Secret);
 
                 var tokenDescritor = new SecurityTokenDescriptor
                 {
@@ -26,7 +26,7 @@ namespace GymScheduleBackend.Services
                         new[]
                         {
                             new Claim(ClaimTypes.Name, user.Nome), // User.Identity.Name
-                            new Claim(ClaimTypes.Role, user.RoleName)
+                            new Claim(ClaimTypes.Role, user.Nome)
                         }),
 
                     Expires = DateTime.UtcNow.AddMinutes(120),
@@ -74,7 +74,7 @@ namespace GymScheduleBackend.Services
                         ValidateAudience = false,
                         ValidateIssuer = false,
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Settings.Settings.Secret)),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Settings.Secret)),
                         ValidateLifetime = false
                     };
                     var tokenHandler = new JwtSecurityTokenHandler();
