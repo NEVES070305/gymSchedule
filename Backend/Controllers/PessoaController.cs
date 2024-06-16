94dd750cf5f1a20d5b08f1c457241035edb468fb
Aguinaldo Mendes <aguinaldomendes.jr@gmail.com>
2024-06-16 18:58:10 -0300

    feat: add new view Apagarconfirmacao

--- Backend/Controllers/PessoaController.cs
+++ Backend/Controllers/PessoaController.cs
@@ -1,16 +1,19 @@
-﻿using Backend.Repository;
+﻿using Backend.Data;
+using Backend.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace Backend.Controllers
 {
     public class PessoaController : Controller
     {
         private readonly PessoaRepository pessoaRepository;
-
+        private readonly ApplicationDbContext context;
         // Inicializa o EnderecoRepository através do construtor
-        public PessoaController(PessoaRepository pessoaRepository)
+        public PessoaController(PessoaRepository pessoaRepository, ApplicationDbContext context)
         {
             this.pessoaRepository = pessoaRepository;
+            this.context = context;
         }
         public async Task<IActionResult> Index()
         {
@@ -42,10 +45,31 @@ namespace Backend.Controllers
             Pessoa pessoa = pessoaRepository.BuscarPorID(cpf);
             return View(pessoa);
         }
-        public IActionResult Apagar()
+        public IActionResult Apagar(int id)
         {
-            return View();
+            try
+            {
+                bool apagado = pessoaRepository.Apagar(id);
+
+                if (apagado) TempData["MensagemSucesso"] = "Contato apagado com sucesso!"; else TempData["MensagemErro"] = "Ops, não conseguimos cadastrar seu contato, tente novamante!";
+                return RedirectToAction("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar seu contato, tente novamante, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
+            public async Task<IActionResult> ApagarConfirmacao(int id)
+            {
+                var pessoa = await context.Pessoas.FindAsync(id);
+                if (pessoa == null)
+        {
+            return NotFound();
+        }
+        return View(pessoa);
+    }
+
         public IActionResult Cadastrar()
         {
             return View();
