b1b05d63fd5961cb9ba6ea636911df30c8e9ab77
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-06-16 22:09:19 -0300

    feat: added edit and delete to rede

--- Backend/Repository/RedeRepository.cs
+++ Backend/Repository/RedeRepository.cs
@@ -1,13 +1,11 @@
 ﻿using Backend.Data;
+using Backend.Models;
 
 namespace Backend.Repository
 {
     public class RedeRepository
     {
         private readonly ApplicationDbContext _applicationDbContext; 
-        public RedeRepository()
-        {
-        }
         public RedeRepository(ApplicationDbContext applicationDbContext)
         {
             _applicationDbContext = applicationDbContext;
@@ -17,6 +15,43 @@ namespace Backend.Repository
             _applicationDbContext.Redes.Add(rede);
             _applicationDbContext.SaveChanges();
             return rede;
+        }
+        public List<Rede> Listar()
+        {
+            return _applicationDbContext.Redes.ToList();
+        }
+
+        public Rede Editar(RedeUpdateModel rede)
+        {
+            Rede redeDB = BuscarPorCnpj(rede.Cnpj);
+
+            if (redeDB == null) throw new Exception("Houve um erro na atualização da rede!");
+
+            // Atualize apenas os campos especificados
+            redeDB.Cnpj = redeDB.Cnpj;
+            redeDB.Nome = rede.Nome;
+            redeDB.EnderecoId = rede.EnderecoId;
+
+            _applicationDbContext.Redes.Update(redeDB);
+            _applicationDbContext.SaveChanges();
+
+            return redeDB;
+        }
+        public Rede? BuscarPorCnpj(string cnpj)
+        {
+            return _applicationDbContext.Redes.FirstOrDefault(x => x.Cnpj == cnpj);
+        }
+
+        public bool Apagar(string cnpj)
+        {
+            Rede redeDB = BuscarPorCnpj(cnpj);
+
+            if (redeDB == null) throw new Exception("Houve um erro na deleção do contato!");
+
+            _applicationDbContext.Redes.Remove(redeDB);
+            _applicationDbContext.SaveChanges();
+
+            return true;
         }
     }
 }
