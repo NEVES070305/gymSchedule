0ae8e47364405aba4dadac5794da985f60c83280
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-06-16 22:17:37 -0300

    feat: added redecontroller

--- Backend/Controllers/RedeController.cs
+++ Backend/Controllers/RedeController.cs
@@ -1,23 +1,70 @@
-﻿using Backend.Repository;
+﻿using Backend.Models;
+using Backend.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
 {
     public class RedeController : Controller
     {
-        RedeRepository redeRepository = new();
-        public IActionResult Index()
+        private readonly RedeRepository redeRepository;
+        public RedeController(RedeRepository redeRepository){
+            this.redeRepository = redeRepository;
+        }
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var redes = redeRepository.Listar();
+            return View(redes);
         }
 
-        public IActionResult Editar()
+        public IActionResult Editar(string cnpj)
+        {
+            var rede = redeRepository.BuscarPorCnpj(cnpj);
+            if (rede == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new RedeUpdateModel
+            {
+                Cnpj = rede.Cnpj,
+                Nome = rede.Nome,
+                EnderecoId = rede.EnderecoId
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(RedeUpdateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                redeRepository.Editar(model);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View(model);
+            }
+        }
+        public IActionResult Cadastrar()
         {
             return View();
         }
-        public IActionResult ApagarConfirmacao()
+        public ActionResult ApagarConfirmacao(string cnpj)
         {
-            return View();
+            var rede = redeRepository.BuscarPorCnpj(cnpj);
+            if (rede == null)
+            {
+                return View("Index");
+            }
+            return View(rede);
         }
         [HttpPost]
         public IActionResult Criar(Rede rede)
