2edf0ec6c61787180ccfb920513b613eed6eb550
andrelbastida <andrelbastida@yahoo.com.br>
2024-06-17 16:56:28 -0300

    feat: adjusted the models

---
+++ Backend/Models/EnderecoUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class EnderecoUpdateModel
+    {
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+    }
+}
--- Backend/Models/PessoaUpdateModel.cs
+++ Backend/Models/PessoaUpdateModel.cs
@@ -2,7 +2,7 @@
 {
     public class PessoaUpdateModel
     {
-        public int CPF { get; set; }
+        public string CPF { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string UltimoNome { get; set; }
